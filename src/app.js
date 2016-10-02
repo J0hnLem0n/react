74e66b171ab73b3f9466abcc13b657459b40d40b
j0hnLem0n <nikolaevee@gmail.com>
2016-10-02 19:38:12 +0700

    move elements

---
+++ src/app.js
@@ -0,0 +1,2 @@
+import Hello from './jsx/ConnectCard/hello.jsx';
+import World from './jsx/ConnectCard/world.jsx';
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  entry: './app.jsx',
+  entry: './src/app.js',
   output: { path: __dirname, filename: './build/js/app.js' },
   module: {
     loaders: [
